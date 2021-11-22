4b0ca30f9c73a59b7af145cde962fb8ee6cde5b0
martinluther <tutorialcreation81@gmail.com>
2021-11-22 14:45:58 +0300

    added paypal to shopping

--- shop/models.py
+++ shop/models.py
@@ -7,9 +7,8 @@ from django_countries.fields import CountryField
 
 
 CATEGORY_CHOICES = (
-    ('S', 'Shirt'),
-    ('SW', 'Sport wear'),
-    ('OW', 'Outwear')
+    ('B', 'Book'),
+    ('O', 'Outfit')
 )
 
 LABEL_CHOICES = (
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 import random
 import string
+from django.urls.base import reverse
 
 import stripe
 from django.conf import settings
@@ -10,6 +11,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from paypal.standard.forms import PayPalPaymentsForm
 from django.views.generic import ListView, DetailView, View
 
 from .forms import CheckoutForm, CouponForm, RefundForm, PaymentForm
@@ -209,27 +211,47 @@ class CheckoutView(View):
 class PaymentView(View):
     def get(self, *args, **kwargs):
         order = Order.objects.get(user=self.request.user, ordered=False)
+        host = self.request.get_host()
+        amount = int(order.get_total())
         if order.billing_address:
-            context = {
-                'order': order,
-                'DISPLAY_COUPON_FORM': False,
-                'STRIPE_PUBLIC_KEY' : settings.STRIPE_PUBLIC_KEY
+            paypal_dict = {
+                "cmd": "_xclick",
+                'business': settings.PAYPAL_RECEIVER_EMAIL,
+                'amount': amount,
+                'item_name': 'Donate',
+                'custom': 1,     # custom data, pass something meaningful here
+                'currency_code': 'USD',
+                'notify_url': 'http://{}{}'.format(host,
+                                                   reverse('paypal-ipn')),
+                'return_url': 'http://{}{}'.format(host,
+                                                   redirect('donation:success', amount)),
+                'cancel_return': 'http://{}{}'.format(host,
+                                                      reverse('donation:cancel')),
             }
+
+            form = PayPalPaymentsForm(
+                initial=paypal_dict, button_type="buy")
             userprofile = self.request.user.userprofile
-            if userprofile.one_click_purchasing:
-                # fetch the users card list
-                cards = stripe.Customer.list_sources(
-                    userprofile.stripe_customer_id,
-                    limit=3,
-                    object='card'
-                )
-                card_list = cards['data']
-                if len(card_list) > 0:
-                    # update the context with the default card
-                    context.update({
-                        'card': card_list[0]
-                    })
-            return render(self.request, "payment.html", context)
+
+            payment = Payment()
+            payment.user = self.request.user
+            payment.amount = order.get_total()
+            payment.save()
+
+            # assign the payment to the order
+
+            order_items = order.items.all()
+            order_items.update(ordered=True)
+            for item in order_items:
+                item.save()
+
+            order.ordered = True
+            order.payment = payment
+            order.ref_code = create_ref_code()
+            order.save()
+
+            messages.success(self.request, "Your order was successful!")
+            return render(self.request, 'base/purchase.html', locals())
         else:
             messages.warning(
                 self.request, "You have not added a billing address")
@@ -238,49 +260,44 @@ class PaymentView(View):
     def post(self, *args, **kwargs):
         order = Order.objects.get(user=self.request.user, ordered=False)
         form = PaymentForm(self.request.POST)
+        host = self.request.get_host()
         userprofile = UserProfile.objects.get(user=self.request.user)
         if form.is_valid():
-            token = form.cleaned_data.get('stripeToken')
             save = form.cleaned_data.get('save')
             use_default = form.cleaned_data.get('use_default')
 
             if save:
-                if userprofile.stripe_customer_id != '' and userprofile.stripe_customer_id is not None:
-                    customer = stripe.Customer.retrieve(
-                        userprofile.stripe_customer_id)
-                    customer.sources.create(source=token)
 
-                else:
-                    customer = stripe.Customer.create(
-                        email=self.request.user.email,
-                    )
-                    customer.sources.create(source=token)
-                    userprofile.stripe_customer_id = customer['id']
-                    userprofile.one_click_purchasing = True
-                    userprofile.save()
+                userprofile.one_click_purchasing = True
+                userprofile.save()
 
-            amount = int(order.get_total() * 100)
+            amount = int(order.get_total())
 
             try:
 
-                if use_default or save:
-                    # charge the customer because we cannot charge the token more than once
-                    charge = stripe.Charge.create(
-                        amount=amount,  # cents
-                        currency="usd",
-                        customer=userprofile.stripe_customer_id
-                    )
+                if True:
+                    paypal_dict = {
+                        "cmd": "_xclick",
+                        'business': settings.PAYPAL_RECEIVER_EMAIL,
+                        'amount': amount,
+                        'item_name': 'Donate',
+                        'custom': 1,     # custom data, pass something meaningful here
+                        'currency_code': 'USD',
+                        'notify_url': 'http://{}{}'.format(host,
+                                                           reverse('paypal-ipn')),
+                        'return_url': 'http://{}{}'.format(host,
+                                                           redirect('donation:success', amount)),
+                        'cancel_return': 'http://{}{}'.format(host,
+                                                              reverse('donation:cancel')),
+                    }
+
+                    form = PayPalPaymentsForm(
+                        initial=paypal_dict, button_type="buy")
                 else:
-                    # charge once off on the token
-                    charge = stripe.Charge.create(
-                        amount=amount,  # cents
-                        currency="usd",
-                        source=token
-                    )
+                    pass
 
                 # create the payment
                 payment = Payment()
-                payment.stripe_charge_id = charge['id']
                 payment.user = self.request.user
                 payment.amount = order.get_total()
                 payment.save()
@@ -298,42 +315,7 @@ class PaymentView(View):
                 order.save()
 
                 messages.success(self.request, "Your order was successful!")
-                return redirect("/")
-
-            except stripe.error.CardError as e:
-                body = e.json_body
-                err = body.get('error', {})
-                messages.warning(self.request, f"{err.get('message')}")
-                return redirect("/")
-
-            except stripe.error.RateLimitError as e:
-                # Too many requests made to the API too quickly
-                messages.warning(self.request, "Rate limit error")
-                return redirect("/")
-
-            except stripe.error.InvalidRequestError as e:
-                # Invalid parameters were supplied to Stripe's API
-                print(e)
-                messages.warning(self.request, "Invalid parameters")
-                return redirect("/")
-
-            except stripe.error.AuthenticationError as e:
-                # Authentication with Stripe's API failed
-                # (maybe you changed API keys recently)
-                messages.warning(self.request, "Not authenticated")
-                return redirect("/")
-
-            except stripe.error.APIConnectionError as e:
-                # Network communication with Stripe failed
-                messages.warning(self.request, "Network error")
-                return redirect("/")
-
-            except stripe.error.StripeError as e:
-                # Display a very generic error to the user, and maybe send
-                # yourself an email
-                messages.warning(
-                    self.request, "Something went wrong. You were not charged. Please try again.")
-                return redirect("/")
+                return render(self.request, 'base/purchase.html', locals())
 
             except Exception as e:
                 # send an email to ourselves
