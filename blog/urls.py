f52382f4fa7d4f7ba1fabd8de04a77a663df0107
lunyamwis <lutherlunyamwi@gmail.com>
2022-02-01 17:25:27 +0300

    fixed the urls issue from main urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,24 +1,24 @@
+"""
+Date: 01/02/2022
+Author: Martin Luther Bironga
+Purpose: Routing the main urls
+"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import include, path
+
+from marketing.views import email_list_signup
 from posts.views import (
-    index,
+    IndexView,
+    PostCreateView,
+    PostDeleteView,
+    PostDetailView,
+    PostListView,
+    PostUpdateView,
     about,
     search,
-    post_list,
-    post_detail,
-    post_create,
-    post_update,
-    post_delete,
-    IndexView,
-    PostListView,
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteView
 )
-from marketing.views import email_list_signup
 
 # from djangojs.views import JasmineView, QUnitView
 
@@ -29,37 +29,34 @@ from marketing.views import email_list_signup
 #         js_files='js/tests/*.tests.js'), name='my_qunit_view'),
 # ]
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
+    path("admin/", admin.site.urls),
     # path('', index),
-    path('', IndexView.as_view(), name='home'),
-    path('about/', about, name='about'),
+    path("", IndexView.as_view(), name="home"),
+    path("about/", about, name="about"),
     # path('blog/', post_list, name='post-list'),
-    path('blog/', PostListView.as_view(), name='post-list'),
-    path('search/', search, name='search'),
-    path('email-signup/', email_list_signup, name='email-list-signup'),
+    path("blog/", PostListView.as_view(), name="post-list"),
+    path("search/", search, name="search"),
+    path("email-signup/", email_list_signup, name="email-list-signup"),
     # path('create/', post_create, name='post-create'),
-    path('create/', PostCreateView.as_view(), name='post-create'),
+    path("create/", PostCreateView.as_view(), name="post-create"),
     # path('post/<id>/', post_detail, name='post-detail'),
-    path('post/<pk>/', PostDetailView.as_view(), name='post-detail'),
+    path("post/<pk>/", PostDetailView.as_view(), name="post-detail"),
     # path('post/<id>/update/', post_update, name='post-update'),
-    path('post/<pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path("post/<pk>/update/", PostUpdateView.as_view(), name="post-update"),
     # path('post/<id>/delete/', post_delete, name='post-delete'),
-    path('post/<pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('tinymce/', include('tinymce.urls')),
+    path("post/<pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("tinymce/", include("tinymce.urls")),
     path("users/", include("users.urls", namespace="users")),
-    path('accounts/', include('allauth.urls')),
-    path('courses/', include('content.urls')),
-    path('shop/', include('shop.urls')),
-    path('evangelism/', include('evangelism.urls', namespace='evangelism')),
-    path('donation/', include('donation.urls', namespace='donation')),
-    path('payment/', include('payment.urls')),
-    path('payments/', include('payments.urls')),
-    path('paypal/', include("paypal.standard.ipn.urls")),
+    path("accounts/", include("allauth.urls")),
+    path("courses/", include("content.urls")),
+    path("shop/", include("shop.urls")),
+    path("evangelism/", include("evangelism.urls", namespace="evangelism")),
+    path("donation/", include("donation.urls", namespace="donation")),
+    path("payment/", include("payment.urls")),
+    path("payments/", include("payments.urls")),
+    path("paypal/", include("paypal.standard.ipn.urls")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
